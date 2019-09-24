0b1c18b750273629a79c9eead31a7c8fa4b08ad4
alejandro <Alejandro>
2019-09-24 11:43:30 -0500

    clases creadas

--- logo/src/main/java/co/edu/javeriana/logo/ast/Greater.java
+++ logo/src/main/java/co/edu/javeriana/logo/ast/Greater.java
@@ -1,5 +1,25 @@
 package co.edu.javeriana.logo.ast;
 
-public class Greater {
+import java.util.Map;
+
+import co.edu.javeriana.logo.scope.Context;
+
+	public class Greater implements ASTNode{
+		
+		private ASTNode operand1;
+		private ASTNode operand2;
+		
+		
+		public Greater(ASTNode operand1, ASTNode operand2) {
+			super();
+			this.operand1 = operand1;
+			this.operand2 = operand2;
+		}
+
+
+		@Override
+		public Object execute(Context symbolTable) {
+			return (Float)operand1.execute(symbolTable) > (Float)operand2.execute(symbolTable);
+		}
 
 }
---
+++ logo/src/main/java/co/edu/javeriana/logo/ast/LessEqual.java
@@ -0,0 +1,23 @@
+package co.edu.javeriana.logo.ast;
+
+import co.edu.javeriana.logo.scope.Context;
+
+public class LessEqual implements ASTNode {
+	private ASTNode operand1;
+	private ASTNode operand2;
+	
+	
+	public LessEqual(ASTNode operand1, ASTNode operand2) {
+		super();
+		this.operand1 = operand1;
+		this.operand2 = operand2;
+	}
+
+
+	@Override
+	public Object execute(Context symbolTable) {
+		// TODO Auto-generated method stub
+		return (Float)operand1.execute(symbolTable) <= (Float)operand1.execute(symbolTable);
+	}
+
+}
